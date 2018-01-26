5ba22c3323b761582dcc2852afe799a2312d3fa9
Mariah Shipley <mshipley57@gmail.com>
2018-01-26 10:40:41 -0800

    add html, begin js

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//back end logic
+var inputtedPizzaSize = ("");
+var inputtedPizzaTopping
+var inputtedQuantity = 0
+var price = 0
+
+function Pizza(size, topping) {
+  this.size = size;
+  this.topping = topping;
+}
+
+price = function(size,topping,quantity) {
+  var sizePrice = 0;
+  var typePrice = 0;
+  if (size === "Kilobyte") {
+    sizePrice = 8;
+  } else {
+    sizePrice = 10
+  }
+  if (type === "Tesla Coils" || "AI") {
+    typePrice = 1
+  } else {
+    typePrice = .75;
+  }
+  return parseFloat ((typePrice * sizePrice * quantity)).toFixed(2));
+}
+
+//UI logic
+$(document).ready(function(){
+  $("form#pizza").submit(function(event){
+    event.preventDefault();
+
+    inputtedPizzaSize = $("select#pizzaBigness").val();
+    inputtedPizzaTopping = $("select#pizza-toppings").val();
+  }
+}
