2baadb17c8112f6a2619b013d175b7395d1de74f
Mariah Shipley <mshipley57@gmail.com>
2018-01-26 15:47:09 -0800

    simplified program, add readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,44 @@
 //back end logic
 function Pizza(size, topping) {
   this.size = size;
-  this.topping = toppings
+  this.toppings = toppings;
 }
 
 var inputtedPizzaSize = ("");
 var inputtedPizzaTopping = ("");
-var inputtedQuantity = 0;
+var inputtedQuantity = 1;
 var cost = 0;
 
 cost = function(size,topping,quantity) {
-  var sizeCost = 0;
-  var toppingsCost = 0;
+  var sizeCost = (0);
+  var toppingsCost = (0);
   if (size === "Kilobyte") {
-    sizeCost = 8;
-  } else {
-    sizeCost = 10;
+    sizeCost = (8);
+  } else; {
+    sizeCost = (10);
   }
-  if ((toppings === "Tesla Coils") || ("AI")) {
-    toppingsCost = 1;
-  } else {
-    typeCost = .8;
+  if (toppings === "Tesla Coils") {
+    toppingsCost = (1);
+  } else; {
+    typeCost = '.8';
   }
-  append.parseFloat ((typeCost * sizeCost * quantity).toFixed(2));
+
 };
+
 //UI logic
 $(document).ready(function(){
-  $("form#wholePizza").submit(function(event){
+
+  $("form#pizza-size").submit(function(event){
     event.preventDefault();
+    alert("ashfads")
+    $("select#pizza-size-options").val();
+    $("select#pizza-type").val();
+    $("input#pizza-amount").val();
 
-    inputtedPizzaSize = $("select#pizza-size-options").val();
-    inputtedPizzaTopping = $("select#pizza-toppings").val();
-    inputtedQuantity = parseInt($("input#amount")).val();
-    cost = cost(inputtedPizzaSize, inputtedPizzaTopping, inputtedQuantity).toFixed(2);
 
-    $("#pizza-size-options").append(inputtedPizzaSize);
-    $("#pizza-toppings").append(inputtedPizzaTopping);
-    $("#pizza-amount").append(inputtedQuantity);
-    $(".pizza-cost").text("$" + cost);
 
-    $(".pizza-cost").append;
 
-  //  newPizza = new Pizza (inputtedPizzaSize, inputtedPizzaTopping, inputtedQuantity, cost);
-  //  $(".pizza-size-options").text(newPizza.pizza-size-options);
-  //  $("#pizza-toppings").text(newPizza.toppings);
-  //  $("#pizza-amount").text(newPizza.amount);
-  //  $(".pizza-cost").text("$" + cost);
 
-  //  $(".pizza-cost").show();
 
   })
 });
---
+++ readme.md
@@ -0,0 +1,42 @@
+# _{Pizza-Site}_
+
+#### _{An online order form form pizza}, {1-26-18}_
+
+#### By _**{Mariah Shipley}**_
+
+## Description
+
+_{Website that sells kooky pizzas. You can select by size or pizza type, and quantity. After completing your order, you will be given a total cost.}_
+
+##Specifications:
+
+*Program gives options for pizza size
+*Program gives option for pizza topping/type
+*Program takes in quantity of pizza's
+*Program shows cost dependent upon information above
+
+## Setup/Installation Requirements
+
+* _Download page_
+* _Place file_
+* _Connect via browser_
+
+_{This site has nothing a typical browser can't handle}_
+
+## Known Bugs
+
+_{Presently the page is unable to display order information and cost}_
+
+## Support and contact details
+
+_{Feel free to send any questions or comments about this site, email me at mshipley57@gmail.com}_
+
+## Technologies Used
+
+_{HTML, CSS, JavaScript, JQuery.}_
+
+### License
+
+*{Free to use under the GNU General public license (GPL).}*
+
+Copyright (c) 2018 **_{Mariah Shipley}_**
